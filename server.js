6f03240841b7a7012e233d5d439aec28c10f7a33
Márcio Freitas <79476199+MarcioFreitasUFSC21@users.noreply.github.com>
2023-04-11 14:56:36 -0300

    programa punk

--- server.js
+++ server.js
@@ -9,4 +9,15 @@ const app = express();
 const port = 3000;
 app.set('port', port);
 
-console.log ("testing...");
+
+const server = http.createServer(app);
+const router = express.Router();
+
+
+var route = router.get('/' , (req, res, next) => {
+    res.status(200).send ({
+        title: "Node Store API", 
+        version: "0.0.1"
+    });
+});
+
