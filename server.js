eed134314721281ce4df43bd3c4f987e7fc2bdb3
Márcio Freitas <79476199+MarcioFreitasUFSC21@users.noreply.github.com>
2023-04-11 14:43:23 -0300

    terceira comiit

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const http = require('http');
+const debug = require('debug')('baknode:server');
+const express = require('express');
+
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+console.log ("testing...");
