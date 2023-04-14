9a2631b323b1a0e0115e3b05c8cba66a304297a2
Márcio Freitas <79476199+MarcioFreitasUFSC21@users.noreply.github.com>
2023-04-14 12:59:28 -0300

    iniciando os desafios do mongo DB com a API baita no vsCode

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
+//const config = require('config');
 //****************************************************************** */
 //                    Charge routes
 //****************************************************************** */
